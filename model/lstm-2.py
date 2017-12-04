bbc0fe731701da2bbb12ea5680c57837e907e7f9
kaushik shingne <kaushikshingne18@gmail.com>
2017-12-03 18:10:38 -0800

    update model

--- model/lstm-2.py
+++ model/lstm-2.py
@@ -13,7 +13,7 @@ feature_set, class_set, lexicon_size = processor.words_to_vectors()
 
 
 lstm_units = 256
-batch_size = 5
+batch_size = 100
 lstm_layers = 1
 learning_rate = 0.001
 
@@ -36,13 +36,27 @@ print(trainX.shape, trainY.shape)
 lstm_net = tflearn.input_data(shape=[None, lexicon_size, 1])
 trainY = to_categorical(trainY, nb_classes=len(ml_classes))
 #lstm_net = tflearn.embedding(lstm_net)
-lstm_net = tflearn.lstm(lstm_net,weights_init='xavier', n_units=lstm_units, return_seq=True)
-lstm_net = tflearn.lstm(lstm_net,weights_init='xavier', n_units = lstm_units)
+lstm_net = tflearn.lstm(lstm_net,weights_init='xavier', n_units=lstm_units, return_seq=False)
+#lstm_net = tflearn.lstm(lstm_net,weights_init='xavier', n_units = lstm_units)
 lstm_net = tflearn.fully_connected(
     lstm_net, n_units=len(ml_classes), activation='softmax')
 lstm_net = tflearn.regression(lstm_net, optimizer='adam', learning_rate=learning_rate,
                               loss='categorical_crossentropy')
 
 model = tflearn.DNN(lstm_net, tensorboard_verbose=0)
-model.fit(trainX, trainY, show_metric=True, batch_size=batch_size, n_epoch=10)
+model.fit(trainX, trainY, show_metric=True, batch_size=batch_size, n_epoch=25)
 model.save("saved/lstm-model.tfl")
+
+
+while True:
+    tr_data = input('Enter testing string : ')
+    if tr_data != 'q':
+        tr_data = processor.vectorize_input(tr_data)
+        tr_data = np.reshape(tr_data, newshape=[1, lexicon_size, 1])
+        predicted_class = model.predict_label(tr_data)
+        predicted_values = model.predict(tr_data)
+        print(predicted_class)
+        print(predicted_values)
+        print(ml_classes[predicted_class[0][0]], predicted_values[0][0])
+    else:
+        break
